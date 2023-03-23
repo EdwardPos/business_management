a6dd9bf7f082534cbc964984f72ce48a7f9a9732
EdwardPos <edwardpos@gmail.com>
2023-03-23 11:04:18 +0200

    blueprint alembic db

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import models
+from database import engine
+from routers import auth, bm
+from starlette.staticfiles import StaticFiles
+# from routers import auth, bm, address
+# from company import companyapis, dependencies
 
 app = FastAPI()
 
-PROJECT = 'Business Management System'
+models.Base.metadata.create_all(bind=engine)
 
-
-@app.get("/")
-async def proj_name():
-    return f'Starting a PTU8 Project: {PROJECT}'
+app.mount("/static", StaticFiles(directory="static"), name="static")                                                                    
+app.include_router(auth.router)
+app.include_router(bm.router)
+# app.include_router(address.router)
+# app.include_router(
+    # companyapis.router,
+    # prefix="/companyapis",
+    # tags=["companysapis"],
+    # dependencies=[Depends(dependencies.get_token_header)],
+    # responses={418: {"description": "Internal Use Only"}}
+# )
